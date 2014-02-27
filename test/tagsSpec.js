51a4d06c3dfa69e470fa360d00a1acec11214fd4
Jeffrey Cho <jeffcho59@gmail.com>
2014-02-27 02:02:51 -0500

    Added it block to tagsSpec

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -9,15 +9,29 @@ describe("Tags", function(){
  
            expect(results).to.have.a.property("depth", 4);
            expect(results).to.have.a.property("hello", "world");
-           
-      
-		exports = module.exports = {};
-		
-		exports.parse = function() {
-		 
-		}
-		
        });
+       
+		it("should parse long formed tags and convert numbers", function(){
+		    var args = ["--depth=4", "--hello=world"];
+		    var results = tags.parse(args);
+		 
+		    expect(results).to.have.a.property("depth", 4);
+		    expect(results).to.have.a.property("hello", "world");
+		});
+		
+		it("should fallback to defaults", function(){
+		    var args = ["--depth=4", "--hello=world"];
+		    var defaults = { depth: 2, foo: "bar" };
+		    var results = tags.parse(args, defaults);
+		 
+		    var expected = {
+		        depth: 4,
+		        foo: "bar",
+		        hello: "world"
+		    };
+		 
+		    expect(results).to.deep.equal(expected);
+		});
    });
 });
 
